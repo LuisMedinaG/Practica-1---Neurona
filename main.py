25ce86f8491049bf09d237305a7064875fdd5364
Luis Medina G <37083265+LuisMedinaG@users.noreply.github.com>
2021-06-24 08:52:29 +0000

    p4 init

--- main.py
+++ main.py
@@ -4,16 +4,19 @@ from matplotlib.backend_bases import MouseButton
 from tkinter import Button
 import tkinter as tk
 import matplotlib.pyplot as plt
+from typing import List
 import random
+import numpy as np
 
 fig = None
 ax = None
 canvas = None
+TOLERANCIA = 0.1
 
 
 class Application(object):
     def __init__(self):
-        self.perceptron = Perceptron()
+        self.training = Training()
 
     def create_window(self):
         global ax
@@ -27,7 +30,7 @@ class Application(object):
         ax.grid(True)
 
         window = tk.Tk()
-        window.wm_title("Luis Medina - IA2 - Practica 2")
+        window.wm_title("Luis Medina - IA2 - Practica 4")
 
         canvas = FigureCanvasTkAgg(fig, master=window)
         canvas.draw()
@@ -35,10 +38,8 @@ class Application(object):
         wid = canvas.get_tk_widget()
         wid.grid(row=0, column=2, rowspan=50)
 
-        # self.perceptron.setPlt(ax, fig)
-
         Button(window, text="Start",
-               command=self.perceptron.train).grid(row=3,
+               command=self.training.train).grid(row=3,
                                                    column=0,
                                                    columnspan=2)
         Button(window, text="Reset", command=self.reset).grid(row=4,
@@ -49,7 +50,7 @@ class Application(object):
         tk.mainloop()
 
     def reset(self):
-        self.perceptron = Perceptron()
+        self.training = Training()
         ax.clear()
         ax.grid(True)
         ax.set_xlim([-10, 10])
@@ -59,56 +60,66 @@ class Application(object):
     def onclick(self, event):
         x, y = event.xdata, event.ydata
         if event.button == MouseButton.LEFT:
-            self.perceptron.inputs.append([[1, x, y], 0])
+            self.training.inputs.append([[1, x, y], 0])
             ax.plot(event.xdata, event.ydata, color='red', marker='o')
         elif event.button == MouseButton.RIGHT:
-            self.perceptron.inputs.append([[1, x, y], 1])
+            self.training.inputs.append([[1, x, y], 1])
             ax.plot(event.xdata, event.ydata, color='blue', marker='o')
         fig.canvas.draw()
 
 
-class Perceptron:
+# class Neuron:
+#     def __init__(self, inputs: List, weights: List, desired: float):
+#         self.x = inputs
+#         self.d = desired
+#         self.w = weights
+#         self.y = 0
+#         self.error = 0
+
+class Training:
     def __init__(self,
                  no_of_inputs=2,
-                 learning_rate=0.05,
+                 learning_rate=0.1,
                  umbral=0,
-                 max_epochs=5000):
+                 max_epochs=1000):
         self.inputs = []
         self.umbral = umbral
         self.max_epochs = max_epochs
         self.learning_rate = learning_rate  # ETA
         self.weights = [random.uniform(0, 1) for _ in range(no_of_inputs + 1)]
-        # self.weights = [0, 0, 0]
 
-    def predict(self, inputs):
+    def activation(self, y): 
+        # linear activation
+        # return y
+        # logistica
+        return 1 / (1 + np.exp(-y))
+    
+    def net_input(self, inputs):
         # summation = producto_punto(inputs, self.weights[1:]) + self.weights[0]
-        summation = producto_punto(inputs, self.weights)
-        if summation > self.umbral:
-            activation = 1
-        else:
-            activation = 0
-        return activation
+        return producto_punto(inputs, self.weights)
 
     def train(self):
-        # self.inputs = [((1, 0, 0), 1), ((1, 0, 1), 1), ((1, 1, 0), 1),
-        #                ((1, 1, 1), 0)]
-
         epochs = 0
+        costos = []
         while True:
             print('-' * 60)
-            err_count = 0
+            errors = []
             for vector_de_entrada, salida_deseada in self.inputs:
                 print(self.weights)
 
-                prediction = self.predict(vector_de_entrada)
+                prediction = self.activation(self.net_input(vector_de_entrada))
                 error = salida_deseada - prediction
+                
+                for i, valor in enumerate(vector_de_entrada):
+                    self.weights[i] += self.learning_rate * error * valor
 
-                if error != 0:
-                    err_count += 1
-                    for idx, valor in enumerate(vector_de_entrada):
-                        self.weights[idx] += self.learning_rate * error * valor
+                errors.append(error)
 
-            if err_count == 0:
+            cost = self.sum_squares(errors)
+            costos.append(cost)
+            print(cost)
+            
+            if cost < TOLERANCIA:
                 print("Exito! Entrenamiento finalizado.")
                 break
 
@@ -120,6 +131,19 @@ class Perceptron:
             self.draw_line()
 
             epochs += 1
+
+        # plt.plot(range(1, len(costos) + 1), costos)
+        # plt.title("Adaline: learn-rate 0.001")
+        # plt.xlabel('Epochs')
+        # plt.ylabel('Cost (Sum-of-Squares)')
+        # plt.show()
+
+    def sum_squares(self, errors):
+        cost = 0
+        for e in errors:
+            cost += e**2
+        return cost / 2.0
+
 
     def draw_line(self):
         global fig
@@ -151,7 +175,7 @@ class Perceptron:
             else:
                 ax.plot(x, y, 'bo')
 
-        print(x_list, y_list)
+        # print(x_list, y_list)
         ax.plot(x_list, y_list, 'g')
         fig.canvas.draw()
         plt.pause(0.5)
